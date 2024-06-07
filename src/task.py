236b8fb46b5b07322f24a38b317e95f337915b8f
Nathaniel Robinson <nathanielpaulrobinson@gmail.com>
2024-06-07 14:39:35 -0400

    Night Light Source Change
    Change NL source to calibrated collection from Awesome GEE. Removed the pre-processing step. Added latitudinal thresholding to the main script. Modified quantile values.

--- src/task.py
+++ src/task.py
@@ -7,57 +7,79 @@ from input_preprocess import HIIPowerPreprocessTask
 
 class HIIPower(HIITask):
     inputs = {
-        "dmsp_viirs_calibrated": {
+        "nightlights": {
             "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": "projects/HII/v1/source/nightlights/dmsp_viirs_calibrated",
+            "ee_path": "nightlights_path",
             "maxage": 1,
-        },
+        }
     }
+
     quantiles = {
-        "0": {"value": 0, "min": 0, "max": 0},
-        "1": {"value": 1, "min": 1, "max": 4},
-        "2": {"value": 2, "min": 5, "max": 5},
-        "3": {"value": 3, "min": 6, "max": 6},
-        "4": {"value": 4, "min": 7, "max": 7},
-        "5": {"value": 5, "min": 8, "max": 9},
-        "6": {"value": 6, "min": 10, "max": 11},
-        "7": {"value": 7, "min": 12, "max": 16},
-        "8": {"value": 8, "min": 17, "max": 30},
-        "9": {"value": 9, "min": 31, "max": 62},
+        "0": {"value": 0, "min": 0, "max": 9},
+        "1": {"value": 1, "min": 10, "max": 10},
+        "2": {"value": 2, "min": 11, "max": 12},
+        "3": {"value": 3, "min": 13, "max": 13},
+        "4": {"value": 4, "min": 14, "max": 16},
+        "5": {"value": 5, "min": 17, "max": 19},
+        "6": {"value": 6, "min": 20, "max": 25},
+        "7": {"value": 7, "min": 26, "max": 35},
+        "8": {"value": 8, "min": 36, "max": 51},
+        "9": {"value": 9, "min": 52, "max": 62},
         "10": {"value": 10, "min": 63, "max": 63},
+    }
+
+    thresholds = {
+        "global": 10,
+        "latitude": {"min_lat": 45, "min_val": 10, "max_lat": 60, "max_val": 30},
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.nightlights, _ = self.get_most_recent_image(
+            ee.ImageCollection(self.inputs["nightlights"]["ee_path"])
+        )
         self.quantiles = ee.Dictionary(self.quantiles)
 
-    @property
-    def nightlights(self):
-        # If nightlights for previous year has already been calculated and stored, use it
-        nightlights_ic = ee.ImageCollection(
-            self.inputs["dmsp_viirs_calibrated"]["ee_path"]
-        )
-        nightlights, _ = self.get_most_recent_image(nightlights_ic)
-        if nightlights:
-            taskyear = self.taskdate.year
-            nightlightsyear = _.get("year").getInfo()
-            if (
-                0
-                <= (taskyear - nightlightsyear)
-                <= self.inputs["dmsp_viirs_calibrated"]["maxage"]
-            ):
-                return nightlights
+    def nightlights_path(self):
+        if self.taskdate.year < 2013:
+            return "projects/sat-io/open-datasets/Harmonized_NTL/dmsp"
+        else:
+            return "projects/sat-io/open-datasets/Harmonized_NTL/viirs"
 
-        # otherwise calculate calibrated viirs for previous year, Jan 1 - Dec 31
-        prevyear = self.taskdate.year - 1
-        print(f"calc calibrated lights for {prevyear}")
-        lastyearviirsdate = date(year=prevyear, month=12, day=31).isoformat()
-        calibrated_viirs_task = HIIPowerPreprocessTask(taskdate=lastyearviirsdate)
-        calibrated_viirs_task.run()
-        nightlights, _ = self.get_most_recent_image(nightlights_ic)
-        return nightlights
+    def latitude_mask(self):
+        min_lat = self.thresholds["latitude"]["min_lat"] ** 4
+        max_lat = self.thresholds["latitude"]["max_lat"] ** 4
+        min_val = self.thresholds["latitude"]["min_val"]
+        max_val = self.thresholds["latitude"]["max_val"]
+
+        latitude_image = (
+            ee.Image.pixelLonLat()
+            .select("latitude")
+            .abs()
+            .pow(4)
+            .clamp(min_lat, max_lat)
+            .divide(max_lat)
+        )
+        latitude_threshold_image = (
+            latitude_image.subtract(min_lat / max_lat)
+            .divide(1 - min_lat / max_lat)
+            .multiply(max_val - min_val)
+            .add(min_val)
+            .rename("latitude_thresholds")
+        )
+        return latitude_threshold_image
 
     def calc(self):
+        if self.taskdate.year < 2013:
+            self.nightlights = self.nightlights.updateMask(
+                self.nightlights.gte(self.thresholds["global"])
+            )
+
+        else:
+            self.nightlights = self.nightlights.updateMask(
+                self.nightlights.gte(self.latitude_mask())
+            )
+
         def power_classify(value):
             bin = ee.Dictionary(self.quantiles.get(value))
             power_value = ee.Number(bin.get("value"))
@@ -81,10 +103,9 @@ class HIIPower(HIITask):
             .rename("hii_power_driver")
         )
 
-        self.export_image_ee(hii_power_driver, "driver/power")
+        self.export_image_ee(hii_power_driver, "driver/power_alt_nl")
 
     def check_inputs(self):
-        previousyear_nightlights = self.nightlights  # trigger calculation if necessary
         super().check_inputs()
 
 
